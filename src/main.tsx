5294eeaf95e5eb89d8ced6ccd8b4c9032b29b2a3
Maqsood Ahmed <104073101+mqxod@users.noreply.github.com>
2025-10-16 23:46:56 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"; // ✅ Added this
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Analytics /> {/* ✅ Add this below your App */}
+    <SpeedInsights />
+  </>
+);
